d539616c07b12ee00813a33fbaf0f1b6ff356d5a
Aodrey <aodreyhappy@gmail.com>
2014-10-09 00:17:51 +0900

    進んだ

--- src/ipgau/PacketGau.java
+++ src/ipgau/PacketGau.java
@@ -10,8 +10,14 @@ import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
+import java.awt.event.ActionEvent;
 
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 /**
@@ -21,9 +27,15 @@ import javax.swing.JTextField;
 public class PacketGau extends JFrame implements DropTargetListener {
 
 	public static PacketGau frame;
+    private static JPanel contentPane;
+    private static JEditorPane epMsgView;
+    private static JScrollPane mvsp;
+    static JTextField tfMessage;
+    static JTextField tfAddress;
+    private static JButton btnSend;
 
-	private static final int gauWidth = 480;  // ウィンドウ横幅デフォルト
-	private static final int gauHeight = 360;  // ウィンドウ高さデフォルト
+	private static final int gauWidth = 410;  // ウィンドウ横幅デフォルト
+	private static final int gauHeight = 310;  // ウィンドウ高さデフォルト
 	
 	// 表示位置
 	static int frameX;
@@ -51,6 +63,40 @@ public class PacketGau extends JFrame implements DropTargetListener {
         frame.setVisible(true);
 		
 	}
+	
+    /**
+     * コンストラクタ。
+     */
+    public PacketGau() {
+    	//初期化処理
+    	initGau();
+    }
+    /**
+     * 初期化処理
+     */
+    private void initGau() {
+    	
+        // メッセージ表示欄スクロールバー
+        mvsp = new JScrollPane(epMsgView);
+        mvsp.setBounds(10, 10, 373, 190);
+
+        // 送信ボタン
+        btnSend = new JButton("送信");
+        btnSend.setBounds(333, 207, 48, 24);
+        btnSend.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // buttonSend();
+            	System.out.println("そうしん！");
+            }
+        });
+        btnSend.setToolTipText("送信ボタン");
+        
+        // フレーム（パネル）
+        contentPane = (JPanel)this.getContentPane();
+        contentPane.setLayout(null);
+        contentPane.add(mvsp, null);
+        contentPane.add(btnSend, null);
+    }
 
 	/* (non-Javadoc)
 	 * @see java.awt.dnd.DropTargetListener#dragEnter(java.awt.dnd.DropTargetDragEvent)
